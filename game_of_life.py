06ce215698e02543e472681a7e7c870af8f468ae
Javier Acosta <javieracost2@gmail.com>
2015-08-28 14:33:08 -0430

    UI and logic layers separation complete

--- game_of_life.py
+++ game_of_life.py
@@ -21,6 +21,8 @@ class GameThread(QThread):
 class Grid(QGraphicsScene):
 	def __init__(self, tileSize):
 		super(Grid, self).__init__()
+		self.started = False
+		self.running = False
 		self.tileSize = tileSize
 		self.qpen = QPen()
 		self.dead = QBrush(Qt.white)
@@ -31,58 +33,38 @@ class Grid(QGraphicsScene):
 		for x in range(40):
 			for y in range(40):
 				last = self.addRect(QRect(x*self.tileSize, y*self.tileSize, self.tileSize, self.tileSize), self.qpen, self.dead)
+		#test
 		last.setBrush(self.alive)
+
+	def cells(self):
+		return [item for item in self.items() if isinstance(item, QGraphicsRectItem)]
 
 	def mousePressEvent(self,e):
 		clicked = self.itemAt(e.scenePos())
-		#status = (self.dead if clicked.)
+		self.toggle(clicked)
+
+	def toggle(self, cell):
+		newState = (1 if cell.brush() == self.dead else 0)
+		self.setCell(cell, newState)
+
 
 	def setCell(self, cell, alive):
-		#cell.setBrush()
-		pass
-
-		
-class Window(QMainWindow):
-	def __init__(self,ui):
-		super(Window, self).__init__()
-		self.ui = ui
-		self.ui.setupUi(self)
-		self.setFixedSize(420,440)
-		self.ui.actionClose.setShortcut('q')
-		self.ui.actionClose.triggered.connect(qApp.quit)
-		self.ui.actionRestart.setShortcut('r')
-		self.ui.actionRestart.triggered.connect(self.restart)
-		self.ui.actionToggle.setShortcut(' ')
-		self.ui.actionToggle.triggered.connect(self.toggleRun)
-		self.scene = Grid(10)
-		self.ui.graphicsView.setScene(self.scene)
-
-
-		self.started = False
-		self.running = False
-		
-		#some test code
-		#for i in range(10):
-		#	self.setCell(1, i, i)
-		#self.setCell(0,1,1)
-		#self.setCell(0,2,2)
-		#for i in range(10):
-		#	self.setCell(1, i, i)
-
-	def center(self):
-		frameGm = self.frameGeometry()
-		screen = QApplication.desktop().screenNumber(QApplication.desktop().cursor().pos())
-		centerPoint = QApplication.desktop().screenGeometry(screen).center()
-		frameGm.moveCenter(centerPoint)
-		self.move(frameGm.topLeft())
+		state = (self.alive if alive else self.dead)
+		cell.setBrush(state)
 
 	def restart(self):
-		for i in range(40):
-			for j in range(40):
-				self.setCell(0, i, j)
+		for c in self.cells():
+			self.setCell(c, 0)
 		self.running = False
 		self.started = False
 		print "--Game Restarted--"
+
+	def startGame(self):
+		self.threads = [ GameThread(self, q) for q in range(4) ]
+		for t in self.threads:
+			t.start()
+		self.started = True
+		print "Game Started!"
 
 	def toggleRun(self):
 		print "Running status switched"
@@ -95,15 +77,28 @@ class Window(QMainWindow):
 			print "on!"
 			#start/resume code
 		self.running = not self.running
+		
+class Window(QMainWindow):
+	def __init__(self,ui):
+		super(Window, self).__init__()
+		self.ui = ui
+		self.ui.setupUi(self)
+		self.setFixedSize(420,440)
+		self.scene = Grid(10)
+		self.ui.graphicsView.setScene(self.scene)
+		self.ui.actionClose.setShortcut('q')
+		self.ui.actionClose.triggered.connect(qApp.quit)
+		self.ui.actionRestart.setShortcut('r')
+		self.ui.actionRestart.triggered.connect(self.scene.restart)
+		self.ui.actionToggle.setShortcut(' ')
+		self.ui.actionToggle.triggered.connect(self.scene.toggleRun)
 
-	def startGame(self):
-		self.threads = [ GameThread(self.scene, q) for q in range(4) ]
-		for t in self.threads:
-			t.start()
-		self.started = True
-		print "Game Started!"
-
-
+	def center(self):
+		frameGm = self.frameGeometry()
+		screen = QApplication.desktop().screenNumber(QApplication.desktop().cursor().pos())
+		centerPoint = QApplication.desktop().screenGeometry(screen).center()
+		frameGm.moveCenter(centerPoint)
+		self.move(frameGm.topLeft())
 
 qt_app = QApplication(sys.argv)
 window = Window(Ui_MainWindow())
