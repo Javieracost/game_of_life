adec2f0fa142b0375577ff578befb51197f697d3
Javier Acosta <javieracost2@gmail.com>
2015-08-28 02:54:57 -0430

    UI design changed, basic actions implemented Also:     Added simple game logic (start/pause/restart)     first steps towards multithreaded execution

--- game_of_life.py
+++ game_of_life.py
@@ -4,29 +4,48 @@ import sys
 
 from PySide.QtCore import *
 from PySide.QtGui import *
+
 from window import Ui_MainWindow
 
-from time import sleep
+class GameThread(QThread):
+	def __init__(self, scene, quad):
+		QThread.__init__(self)
+		self.scene = scene
+		self.quad = quad
 
+	def run(self):
+		print "created thread to work quadrant: "+str(self.quad)
+		return
 
-#!TODO: Add an abstraction layer between the graphicscene and the game grid
-
-class Window(QMainWindow, QWidget):
+class Window(QMainWindow):
 	def __init__(self,ui):
 		super(Window, self).__init__()
 		self.ui = ui
 		self.ui.setupUi(self)
 		self.setFixedSize(420,440)
+		self.ui.actionClose.setShortcut('q')
+		self.ui.actionClose.triggered.connect(qApp.quit)
+		self.ui.actionRestart.setShortcut('r')
+		self.ui.actionRestart.triggered.connect(self.restart)
+		self.ui.actionToggle.setShortcut(' ')
+		self.ui.actionToggle.triggered.connect(self.toggleRun)
 		self.tileSize = 10
 		self.scene = QGraphicsScene(QRectF(0, 0, 400, 400))
 		self.ui.graphicsView.setScene(self.scene)
-		self.colorPen = QPen(Qt.black)
-		self.whitePen = QPen(Qt.white)
-		for i in xrange(0, 410, self.tileSize):
-			self.scene.addLine(i, 0, i, 400, self.colorPen)
-			self.scene.addLine(0, i, 400, i, self.colorPen)
-		for i in xrange(20):
-			self.turn(1, i, i)
+		self.qpen = QPen()
+		for i in range(0, 410, self.tileSize):
+			self.scene.addLine(i, 0, i, 400, self.qpen)
+			self.scene.addLine(0, i, 400, i, self.qpen)
+		self.started = False
+		self.running = False
+		
+		#some test code
+		for i in range(10):
+			self.setCell(1, i, i)
+		self.setCell(0,1,1)
+		self.setCell(0,2,2)
+		#for i in range(10):
+		#	self.setCell(1, i, i)
 
 	def center(self):
 		frameGm = self.frameGeometry()
@@ -35,25 +54,46 @@ class Window(QMainWindow, QWidget):
 		frameGm.moveCenter(centerPoint)
 		self.move(frameGm.topLeft())
 
-	def turn(self, alive, x, y):
-		"""Sets the tile at x,y coordinates to the given state's color"""
-		if alive:
-			pen = self.colorPen
-		else:
-			pen = self.whitePen
-		self.scene.addRect(QRect(x*self.tileSize, y*self.tileSize, self.tileSize, self.tileSize), pen, QBrush(Qt.SolidPattern))
+	def setCell(self, alive, x, y):
+		"""Sets the cell at x,y to the given state's color"""
+		xpos = x*self.tileSize
+		ypos = y*self.tileSize
+		brush = QBrush(Qt.SolidPattern)
+		if not alive:
+			brush.setColor(Qt.white)
+		self.scene.addRect(QRect(xpos, ypos, self.tileSize, self.tileSize), self.qpen, brush)
+
+	def getCell(self, x, y):
+		"""Returns the state of the cell at x,y"""
+		return ()
 
 	def restart(self):
-		#!TODO Update relevant menu buttons 
-		for i in xrange(40):
-			for j in xrange(40):
-				self.turn(0, i, j)
+		for i in range(40):
+			for j in range(40):
+				self.setCell(0, i, j)
+		self.running = False
+		self.started = False
+		print "--Game Restarted--"
 
-	def keyPressEvent(self, e):
-		if e.key() == Qt.Key_Q:
-			self.close()
-		elif e.key() == Qt.Key_R:
-			self.restart()
+	def toggleRun(self):
+		print "Running status switched"
+		if not self.started:
+			self.startGame()
+		if self.running:
+			#pause code
+			print "off!"
+		else:
+			print "on!"
+			#start/resume code
+		self.running = not self.running
+
+	def startGame(self):
+		self.threads = [ GameThread(self.scene, q) for q in range(4) ]
+		for t in self.threads:
+			t.start()
+		self.started = True
+		print "Game Started!"
+
 
 qt_app = QApplication(sys.argv)
 window = Window(Ui_MainWindow())
--- window.py
+++ window.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'graphic.ui'
 #
-# Created: Tue Aug 25 01:00:26 2015
+# Created: Fri Aug 28 01:24:27 2015
 #      by: pyside-uic 0.2.15 running on PySide 1.2.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -45,21 +45,14 @@ class Ui_MainWindow(object):
         MainWindow.setMenuBar(self.menubar)
         self.actionRestart = QtGui.QAction(MainWindow)
         self.actionRestart.setObjectName("actionRestart")
-        self.actionPause = QtGui.QAction(MainWindow)
-        self.actionPause.setObjectName("actionPause")
-        self.actionResume = QtGui.QAction(MainWindow)
-        self.actionResume.setEnabled(False)
-        self.actionResume.setObjectName("actionResume")
         self.actionClose = QtGui.QAction(MainWindow)
         self.actionClose.setObjectName("actionClose")
         self.actionAbout = QtGui.QAction(MainWindow)
         self.actionAbout.setObjectName("actionAbout")
-        self.actionStart = QtGui.QAction(MainWindow)
-        self.actionStart.setObjectName("actionStart")
-        self.menuFile.addAction(self.actionStart)
+        self.actionToggle = QtGui.QAction(MainWindow)
+        self.actionToggle.setObjectName("actionToggle")
+        self.menuFile.addAction(self.actionToggle)
         self.menuFile.addAction(self.actionRestart)
-        self.menuFile.addAction(self.actionPause)
-        self.menuFile.addAction(self.actionResume)
         self.menuFile.addSeparator()
         self.menuFile.addAction(self.actionClose)
         self.menuHelp.addAction(self.actionAbout)
@@ -75,12 +68,8 @@ class Ui_MainWindow(object):
         self.menuHelp.setTitle(QtGui.QApplication.translate("MainWindow", "&Help", None, QtGui.QApplication.UnicodeUTF8))
         self.actionRestart.setText(QtGui.QApplication.translate("MainWindow", "Restart", None, QtGui.QApplication.UnicodeUTF8))
         self.actionRestart.setToolTip(QtGui.QApplication.translate("MainWindow", "Clear all tiles", None, QtGui.QApplication.UnicodeUTF8))
-        self.actionPause.setText(QtGui.QApplication.translate("MainWindow", "Pause", None, QtGui.QApplication.UnicodeUTF8))
-        self.actionPause.setToolTip(QtGui.QApplication.translate("MainWindow", "Pause game", None, QtGui.QApplication.UnicodeUTF8))
-        self.actionResume.setText(QtGui.QApplication.translate("MainWindow", "Resume", None, QtGui.QApplication.UnicodeUTF8))
-        self.actionResume.setToolTip(QtGui.QApplication.translate("MainWindow", "Resume paused game", None, QtGui.QApplication.UnicodeUTF8))
         self.actionClose.setText(QtGui.QApplication.translate("MainWindow", "Close", None, QtGui.QApplication.UnicodeUTF8))
         self.actionAbout.setText(QtGui.QApplication.translate("MainWindow", "About", None, QtGui.QApplication.UnicodeUTF8))
-        self.actionStart.setText(QtGui.QApplication.translate("MainWindow", "Start", None, QtGui.QApplication.UnicodeUTF8))
-        self.actionStart.setToolTip(QtGui.QApplication.translate("MainWindow", "Start Game", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionToggle.setText(QtGui.QApplication.translate("MainWindow", "Start / Pause", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionToggle.setToolTip(QtGui.QApplication.translate("MainWindow", "Start Game", None, QtGui.QApplication.UnicodeUTF8))
 
